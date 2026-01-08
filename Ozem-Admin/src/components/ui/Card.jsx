183518d8276e5b96cd1f6fd51dc24e8fb7a2e44f
OZME Production <production@ozme.local>
2026-01-08 20:46:01 +0000

    Remove all files from repository

--- Ozem-Admin/src/components/ui/Card.jsx
+++
@@ -1,9 +0,0 @@
-const Card = ({ children, className = '' }) => {
-  return (
-    <div className={`bg-white dark:bg-gray-800 rounded-lg shadow-sm border border-gray-200 dark:border-gray-700 ${className}`}>
-      {children}
-    </div>
-  );
-};
-
-export default Card;
