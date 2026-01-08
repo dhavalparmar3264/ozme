7bf2f41179a28794a54dab315b318a09fcd332d6
OZME Production <production@ozme.local>
2026-01-08 07:17:30 +0000

    Remove all files from repository

--- ozme-backend/src/routes/policyRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getPolicy } from '../controllers/policyController.js';
-
-const router = express.Router();
-
-router.get('/:type', getPolicy);
-
-export default router;
-
